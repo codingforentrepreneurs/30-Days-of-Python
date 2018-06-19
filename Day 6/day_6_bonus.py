954150dd1110dc27b4b68366b11eb699e903c850
akashsharma99 <akash.asharma.sharma11@gmail.com>
2018-06-19 23:08:08 +0530

    Update day_6_bonus.py

--- Day 6/day_6_bonus.py
+++ Day 6/day_6_bonus.py
@@ -7,8 +7,8 @@ default_amounts = [123.32, 94.23, 124.32, 323.4, 23, 322.122323, 32.4, 99.99]
 unf_message = """Hi {name}! 
 
 Thank you for the purchase on {date}. 
-We hope you are exicted about using it. Just as a
-reminder the purcase total was ${total}.
+We hope you are excited about using it. Just as a
+reminder, the purchase total was ${total}.
 Have a great one!
 
 Team CFE
